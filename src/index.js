4f2667cca92d6da3823858ff081bbfdb5ea939c8
Ben Pearlstine <ben.pearlstine@gmail.com>
2020-04-13 22:26:40 -0400

    add in more html to App function

--- src/index.js
+++ src/index.js
@@ -6,7 +6,16 @@ import ReactDOM from 'react-dom';
 
 // Create a react component
 const App = () => {
-    return <div>Hi there!</div>;
+    const buttonText = 'Click me';
+    return (
+        <div>
+            <label className="label" htmlFor="name">Enter name:</label>
+            <input id="name" type="text" />
+            <br />
+            <button style={{ backgroundColor: 'blue', color:'white' }}>
+                {buttonText}
+            </button>
+        </div>);
 }
 
 // Take the react component and show it on the screen
