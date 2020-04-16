50179b5a5d6d79286fab5de2900c15dea5d5e844
Ben Pearlstine <ben.pearlstine@gmail.com>
2020-04-15 22:17:24 -0400

    nesting components, passing component in props

--- src/index.js
+++ src/index.js
@@ -1,14 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import faker from 'faker';
+import ApprovalCard from './ApprovalCard';
 import CommentDetail from './CommentDetail';
 
 const App = () => {
     return (
         <div className="ui container comments">
-            <CommentDetail author={faker.name.firstName()} date={faker.date.weekday()} text={faker.lorem.paragraph()}/>
-            <CommentDetail author={faker.name.firstName()} date={faker.date.weekday()} text={faker.lorem.paragraph()}/>
-            <CommentDetail author={faker.name.firstName()} date={faker.date.weekday()} text={faker.lorem.paragraph()}/>
+            <ApprovalCard>
+                <CommentDetail 
+                    avatar={faker.image.avatar()}
+                    author={faker.name.firstName()}
+                    date={faker.date.weekday()}
+                    text={faker.lorem.paragraph()}/>
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail 
+                    avatar={faker.image.avatar()}
+                    author={faker.name.firstName()}
+                    date={faker.date.weekday()}
+                    text={faker.lorem.paragraph()}/>
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail 
+                    avatar={faker.image.avatar()}
+                    author={faker.name.firstName()}
+                    date={faker.date.weekday()}
+                    text={faker.lorem.paragraph()}/>
+            </ApprovalCard>
         </div>
     );
 }
