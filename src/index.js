7a1b40a4a63594c0e03aba2c098d7a74a908b519
Ben Pearlstine <ben.pearlstine@gmail.com>
2020-04-26 15:06:47 -0400

    lots of refactoring to use conditional rendering and componentDidMount

--- src/index.js
+++ src/index.js
@@ -7,25 +7,36 @@ import ReactDOM from 'react-dom';
 
 
 class App extends React.Component {
-    // js version of __init__
-    constructor(props) {
-        super(props);
 
-        this.state = { lat: null };
+    // sweet sweet class attributes
+    state = { lat: null, errorMessage: '' };
 
+    // componentDidMount() is invoked immediately after a component is mounted (inserted into the tree).
+    // Initialization that requires DOM nodes should go here. If you need to load data from a remote endpoint,
+    // this is a good place to instantiate the network request.
+    // https://reactjs.org/docs/react-component.html#componentdidmount
+    componentDidMount(){
         window.navigator.geolocation.getCurrentPosition(
-            (position) => {
+            position => {
                 // always use setState to update this.state
                 this.setState({ lat: position.coords.latitude})
             },
-            (err) => console.log(err) 
-        );   
+            err => {
+                this.setState({ errorMessage: err.message });
+            }
+        );  
     }
 
     // React says we have to define render
     // or your page won't... render
     render(){ 
-        return <div> Latitude: {this.state.lat}</div>;
+        if (this.state.errorMessage && !this.state.lat) {
+            return <div>Error: {this.state.errorMessage}</div>;
+        } else if (!this.state.errorMessage && this.state.lat) {
+            return <div>latitude: {this.state.lat}</div>;
+        } 
+
+        return <div>Loading!</div>;
     }
 }
 
